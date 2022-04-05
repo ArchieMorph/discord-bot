0aceb3d5a42c69d63610643b3fbabe5215f2181d
brontosol <solanabronto@gmail.com>
2022-04-05 09:11:36 +0700

    part 2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const TOKEN = "OTYwNjA1MDYzMjE3MjgzMTAy.Yks3Lg.9-3GvTxRs9rRavY0sZA-lL9rEkg"
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -18,6 +21,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "LFG"){
         message.reply("TO THE MOON")
     }
+})
+
+const welcomeChannelId = "922768239824633872"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
